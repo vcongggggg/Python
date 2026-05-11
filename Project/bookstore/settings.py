7040c457013602d90e39df94b69a92eb0452ecf9
vcongggggg <vancong030809@gmail.com>
2026-05-11 18:39:22 +0700

    chore: real setup environment variables and secure settings

--- Project/bookstore/settings.py
+++ Project/bookstore/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smart_bookstore',   # tên database MySQL
-        'USER': 'root',              # tài khoản MySQL
-        'PASSWORD': '123456789',      # mật khẩu MySQL
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {
             'charset': 'utf8mb4',
         },
